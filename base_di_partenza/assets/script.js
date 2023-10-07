7c0956bf3fedb32a40c240bf7390916a8b4ad8eb
marchese89 <marchese89@hotmail.com>
2023-10-07 13:38:55 +0200

    modifiche dopo la correzione

--- base_di_partenza/assets/script.js
+++ base_di_partenza/assets/script.js
@@ -5,12 +5,37 @@ window.addEventListener("scroll", function () {
   if (posizioneScroll > 338) {
     if (!navbar.classList.contains("changed")) {
       navbar.classList.add("changed");
-      console.log("evento innescato", window.scrollY);
     }
   } else {
     if (navbar.classList.contains("changed")) {
       navbar.classList.remove("changed");
-      console.log("secondo evento innescato", window.scrollY);
     }
   }
 });
+
+let mm = [];
+
+mm.push(document.querySelector("svg > g > g > g:nth-child(12)"));
+mm.push(document.querySelector("svg > g > g > g:nth-child(354)"));
+mm.push(document.querySelector("svg > g > g > g:nth-child(450)"));
+mm.push(document.querySelector("svg > g > g > g:nth-child(522)"));
+mm.push(document.querySelector("svg > g > g > g:nth-child(430)"));
+mm.push(document.querySelector("svg > g > g > g:nth-child(334)"));
+
+mm = Array.from(mm);
+
+const elements0 = document.querySelectorAll("svg > g > g > g[opacity='0']");
+console.log(elements0);
+
+const elements1 = document.querySelectorAll("svg > g > g > g[opacity='1']");
+console.log(elements1);
+
+setInterval(() => {
+  console.log("chiamato interval");
+  let v = mm[Math.floor(Math.random() * mm.length)];
+  if (v.style.display === "block") {
+    v.style.display = "none";
+  } else {
+    v.style.display = "block";
+  }
+}, 500);
